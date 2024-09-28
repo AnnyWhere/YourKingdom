e8cc19f1fecbebcdcfa898f0ced01cca7fa828e1
AnnyWhere <jukanna2006@gmail.com>
2024-09-28 15:44:16 +0300

    Version 1.0

--- main.py
+++ main.py
@@ -88,6 +88,7 @@ class Kingdom(QMainWindow):
         self.gender = self.new_gender.currentText()
         self.id = count_residents()
         new(self.id, self.name, self.gender, self.image)
+        self.BackToMain()
     def NextPerson(self):
         self.Money.display(loadMoney())
         self.id = (self.id+1) % count_residents()
@@ -98,50 +99,58 @@ class Kingdom(QMainWindow):
         self.Health.setText(str(self.health))
         self.Hunger.setText(str(self.hunger))
         self.NamePerson.setText(self.name)
+        self.Dialog.setText(["Long life and prosperity, Your Majesty!", "Thank You for Your presence, Your Majesty!"][randint(0, 1)]) if self.Person else self.Dialog.setText(". . . \n\nThis resident died")
     def BackToMain(self):
         self.stackedWidget.setCurrentIndex(0)
     def ChangeAppearance(self):
         self.image = (self.image + 1)%4
         self.imagePerson.setPixmap(QPixmap('Person'+str(self.image+1)+'.png'))
     def Work(self):
-        res = int(self.Fatigue.text()) + 10
-        if res > 100:
+        self.fatigue = self.fatigue + 10
+        if self.fatigue > 100:
+            self.fatigue = 100
             self.Change_health()
-        elif res < 101 and self.Person:
-            self.Fatigue.setText(str((res if res < 100 else 100) if res > 0 else 0))
+        elif self.fatigue < 101 and self.Person:
+            self.Fatigue.setText(str(self.fatigue))
             self.Money.display(self.Money.intValue() + 20)
             saveMoney(self.Money.intValue())
             self.Hungry()
             save(self.hunger, self.fatigue, self.health, self.Person, self.id)
     def Sleep(self):
         if self.Person:
-            res = int(self.Fatigue.text()) - 30
-            self.Fatigue.setText(str((res if res < 100 else 100) if res > 0 else 0))
+            self.fatigue = self.fatigue - 30
+            self.fatigue = self.fatigue if self.fatigue > 0 else 0
+            self.Fatigue.setText(str(self.fatigue))
             self.Hungry()
-            self.Dialog.setText(["With your permission, I'll rest for a while.", "Thank you for your concern, Your Majesty!", "I will gain strength and I will definitely repay you for this favor, Your Majesty!"][randint(0, 2)])
+            save(self.hunger, self.fatigue, self.health, self.Person, self.id)
+            self.Dialog.setText(["With Your permission, I'll rest for a while.", "Thank You for your concern, Your Majesty!", "I will gain strength and I will definitely repay You for this favor, Your Majesty!"][randint(0, 2)])
     def Feed(self):
         if self.Person:
-            res = int(self.Hunger.text()) - 30
             if self.Money.intValue() >= 20:
-                self.Hunger.setText(str((res if res < 100 else 100) if res > 0 else 0))
+                self.hunger -= 30
+                self.hunger = self.hunger if self.hunger > 0 else 0
+                self.Hunger.setText(str(self.hunger))
                 self.Money.display(self.Money.intValue() - 20)
-                self.Dialog.setText(["Thank you for your generosity, Your Majesty!", "Oh, thank you, Your Majesty! You are so kind!", "Thanks to you, my family will not starve! Thank you, Your Majesty!"][randint(0, 2)])
+                self.Dialog.setText(["Thank You for Your generosity, Your Majesty!", "Oh, thank You, Your Majesty! You are so kind!", "Thanks to You, my family will not starve! Thank You, Your Majesty!"][randint(0, 2)])
     def Hungry(self):
-        res = int(self.Hunger.text()) + 5
-        if res > 100:
+        self.hunger += 5
+        if self.hunger > 100:
+            self.hunger = 100
             self.Change_health()
-        else:
-            self.Hunger.setText(str((res if res < 100 else 100) if res > 0 else 0))
+        self.Hunger.setText(str(self.hunger))
+        save(self.hunger, self.fatigue, self.health, self.Person, self.id)
     def Change_health(self):
-        res = int(self.Health.text()) - 5
-        if res <= 0:
+        self.health -= 5
+        if self.health <= 0:
+            self.health = 0
+            self.hunger = 0
+            self.fatigue = 0
+            self.Person = 0
             self.Dialog.setText(". . . \n\nThis resident died")
-            self.Person = False
             self.Hunger.setText("0")
             self.Fatigue.setText("0")
-            self.Health.setText("0")
-        else:
-            self.Health.setText(str(int(self.Health.text()) - 5))
+        self.Health.setText(str(self.health))
+        save(self.hunger, self.fatigue, self.health, self.Person, self.id)
 
 
 
